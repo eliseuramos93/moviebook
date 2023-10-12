6bf8e5c20a36f5584cb22e9dc80b54db9b567fa6
Eliseu Ramos <eliseu.mec@gmail.com>
2023-10-12 18:45:00 -0300

    add main routes to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  resources :movie_genres, only: [:new, :create, :edit, :update, :index]
+  resources :movies, only: [:new, :create, :edit, :update, :index]
+  resources :directors, only: [:new, :create, :edit, :update, :index]
+
+  root_to "home#index"
 end
